1df429cea4e99b077c872d784eebb1dde6ae8284
renjithmp <renjithmac@gmail.com>
2019-10-10 16:39:58 +0530

    Three changes 1. Included Swagger reference 2. Added link for Swagger UI/Swagger documentation 3. Increased the time out of npm build .  core 3 projects take some time to build client code

--- source/PerchedPeacockWebApplicationSln/PerchedPeacockWebApplication/Startup.cs
+++ source/PerchedPeacockWebApplicationSln/PerchedPeacockWebApplication/Startup.cs
@@ -11,6 +11,8 @@ using PerchedPeacockWebApplication.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace PerchedPeacockWebApplication
 {
@@ -45,11 +47,26 @@ namespace PerchedPeacockWebApplication
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Perched Peacock API", Version = "v1" });
+            });
+            //  services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Perched Peacock V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -88,6 +105,7 @@ namespace PerchedPeacockWebApplication
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
+                spa.Options.StartupTimeout = new TimeSpan(0, 2, 360);
 
                 if (env.IsDevelopment())
                 {
